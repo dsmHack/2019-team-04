009310a0bcfccfffd97fcd73655cc6690d914dc4
Angela McMahon <angelam@iastate.edu>
2019-02-22 16:39:56 -0600

    Add shortcode for enrollment status

--- wp-content/plugins/bbbs-enrollment/files/bbbs-enrollment.php
+++ wp-content/plugins/bbbs-enrollment/files/bbbs-enrollment.php
@@ -1,8 +1,22 @@
 <?php
-function enrollment_status( $params ) {
+/**
+ * Plugin Name: BBBS Enrollment
+ */
 
-	return "<h1>enrollment</h1>"; // has to return all the HTML
+/**
+ * Shortcode for checking enrollment status for a specific form id
+ * for the currently logged in user.
+ *
+ * @param int $formid Form id for specific form.
+ */
+function enrollment_status( $formid ) {
+	// Check if user is logged in.
+	$user_id = get_current_user_id();
+	if ($user_id == 0) {
+    	echo 'You are currently not logged in.';
+	} else {
+		$returnval = get_entries($formid, $search_criteria['field_filters'][] = array( 'key' => 'created_by', 'value' => $current_user->ID ));
+		return $returnval;
+	}
 }
 add_shortcode( 'enrollmentstatus', 'enrollment_status' );
-
-?>
