07f11f5b662dacd11790ad1f67e5f293dadd4335
nleeper <nick.leeper@gmail.com>
2019-02-22 16:59:38 -0600

    Add BBBS admin menu

--- wp-content/plugins/bbbs-enrollment/index.php
+++ wp-content/plugins/bbbs-enrollment/index.php
@@ -3,6 +3,7 @@
 Plugin Name: BBBS Enrollment
 */
 
+require_once(__DIR__ . "/files/bbbs-admin-reporting.php");
 require_once(__DIR__ . "/files/bbbs-enrollment.php");
 require_once(__DIR__ . "/files/bbbs-install.php");
 require_once(__DIR__ . "/files/bbbs-status.php");
@@ -11,4 +12,6 @@ require_once(__DIR__ . "/files/bbbs-status.php");
 register_activation_hook( __FILE__, 'bbbs_install' );
 add_shortcode( 'enrollmentstatus', 'enrollment_status' );
 add_shortcode( 'status', 'bbbs_status' );
+
+add_action('admin_menu', 'bbbs_volunteer_menu');
 ?>
