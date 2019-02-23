941957fdc8c0dc7b368b7d939066f7feacc1d2e2
Peter Hanson <peter@upandrunningsoftware.com>
2019-02-22 18:06:23 -0600

    updated EnrollmentForms to get volunteer forms and staff forms

--- wp-content/plugins/bbbs-enrollment/files/includes/EnrollmentForms.php
+++ wp-content/plugins/bbbs-enrollment/files/includes/EnrollmentForms.php
@@ -13,18 +13,49 @@ class EnrollmentForms {
         //$rows = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}gf_form", ARRAY_A );
         $forms = GFAPI::get_forms();
         $this->forms = $forms;
-
-
     }
 
-    public function getForms() {
+    public function getAllForms() {
         return $this->forms;
     }
 
-    public function getFormNames() {
+    public function getAllFormNames() {
         return array_map(function($cur) {
             return $cur['title'];
         },$this->forms);
     }
 
+    public function getVolunteerForms() {
+        return array_reduce($this->forms,function($acc,$cur) {
+
+            if ($cur['description'] == "volunteer") {
+                $acc[] = $cur;
+            }
+
+            return $acc;
+        },array());
+    }
+
+    public function getVolunteerFormNames() {
+        return array_map(function($cur) {
+            return $cur['title'];
+        },$this->getVolunteerForms());
+    }
+
+    public function getStaffForms() {
+        return array_reduce($this->forms,function($acc,$cur) {
+
+            if ($cur['description'] == "staff") {
+                $acc[] = $cur;
+            }
+
+            return $acc;
+        },array());
+    }
+
+    public function getStaffFormNames() {
+        return array_map(function($cur) {
+            return $cur['title'];
+        },$this->getStaffForms());
+    }
 }
