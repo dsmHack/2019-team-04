3b824a48f0ebfcd5c256ac66629122f7b94ae7dd
Angela McMahon <angelam@iastate.edu>
2019-02-23 14:39:12 -0600

    Remove template comments

--- html/wp-content/themes/understrap/header.php
+++ html/wp-content/themes/understrap/header.php
@@ -26,7 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="site" id="page">
 <header>
-	<!-- ******************* The Navbar Area ******************* -->
+
 	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
@@ -37,7 +37,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="container">
 		<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
 
 						<?php if ( is_front_page() && is_home() ) : ?>
@@ -53,13 +52,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<?php } else {
 						the_custom_logo();
-					} ?><!-- end custom logo -->
+					} ?>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
-				<!-- The WordPress Menu goes here -->
 				<?php wp_nav_menu(
 					array(
 						'theme_location'  => 'primary',
@@ -73,10 +71,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 					)
 				); ?>
 			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
+			</div>
 			<?php endif; ?>
 
-		</nav><!-- .site-navigation -->
+		</nav>
 
-	</div><!-- #wrapper-navbar end -->
+	</div>
 	</header>
